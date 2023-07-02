ea4f29e060eb9bd9e2c740817c00c63fc2a686b7
a0gr <23467593+a0gr@users.noreply.github.com>
2023-07-02 21:34:55 +1000

    Fix engine directory broken on local testing.

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export GAME_ENGINE_CORE_DIRECTORY=/var/competition
+
 directory=$(realpath $(dirname $(dirname "$0")))
 cd $directory/engine
 ./env/bin/python3 -m engine 2>$directory/output/engine.err
