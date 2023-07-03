f89880c66b6dd80a66d607cc76e2ee4fb721e7bc
Callum Smith <csmi0379@uni.sydney.edu.au>
2023-07-03 16:48:24 +1000

    Added Freeze

--- randyRandomBot/randyRandomBot.py
+++ randyRandomBot/randyRandomBot.py
@@ -14,6 +14,8 @@ class Moveset:
     endTurn = "end turn"
     buyPet = "buy pet"
     buyFood = "buy food"
+    freezeFood = "freeze food"
+    freezePet = "freeze Pet"
     reroll = "reroll"
 
 
@@ -25,20 +27,26 @@ def play_moves(bot_battle: 'BotBattle',
 
     if move == Moveset.buyPet:
         return buy_pet(bot_battle, game_info, player_info)
-    
     elif move  == Moveset.buyFood:
         return buy_food(bot_battle, game_info, player_info)
-    
     elif move == Moveset.endTurn:
         bot_battle.end_turn()
         return True
-
     elif move == Moveset.reroll:
         if player_info.coins() < 1:
             return False
         bot_battle.reroll_shop()
         return True
-    
+    elif move  ==  Moveset.freezeFood:
+        food_to_freeze = player_info.shop_foods()[0]
+        if food_to_freeze.is_frozen == True:
+            return False
+        bot_battle.freeze_food(food_to_freeze)
+    elif move  ==  Moveset.freezePet:
+        pet_to_freeze = player_info.shop_pets()[0]
+        if pet_to_freeze.is_frozen == True:
+            return False
+        bot_battle.freeze_pet(pet_to_freeze)
     else:
         return False
 
@@ -53,22 +61,17 @@ def buy_pet(bot_battle: 'BotBattle', game_info: 'GameInfo', player_info: 'Player
     
     pet_shop_index = random.randint(0,len(player_info.shop_pets()) - 1)
     pet_to_buy = player_info.shop_pets()[pet_shop_index]
-
     bot_battle.buy_pet(pet_to_buy, player_team_slot_index)
     return True
-
 
 def buy_food(bot_battle: 'BotBattle', game_info: 'GameInfo', player_info: 'PlayerInfo'):
     if player_info.coins < 3:
         return False
     
     shop_food = player_info.shop_foods()[0]
-    
     pet_to_feed = player_info.pets()[0]
     bot_battle.buy_food(shop_food, pet_to_feed)
     return True
-
-
 
 
 
@@ -76,7 +79,7 @@ while True:
 
     game_info = bot_battle.get_game_info()
 
-    moveset = [Moveset.buyPet, Moveset.reroll]
+    moveset = [Moveset.buyPet, Moveset.reroll, Moveset.buyFood, Moveset.freezeFood, Moveset.freezePet]
 
     #Always buy a pet first if possible 
     play_moves(bot_battle, game_info, Moveset.buyPet)
